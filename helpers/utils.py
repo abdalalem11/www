7d3043a2657accafe52288257958142a7c01e70c
abd <bwdalmshrqy455@gmail.com>
2026-08-06 12:11:38 +0300

    Update utils.py

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,1 +1,32 @@
+import html
+from telethon.tl.types import MessageEntityMentionName
 
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user and user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or (user and user.startswith("@")):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
+
+class _format:
+    parse_pre = "html"
