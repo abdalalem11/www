5d88d374012a8c8a7e51e79505c99ce72aca646c
abd <bwdalmshrqy455@gmail.com>
2026-08-06 12:13:38 +0300

    Update globals.py

--- sql_helper/globals.py
+++ sql_helper/globals.py
@@ -1,1 +1,32 @@
+import json
+import os
 
+GLOBALS_FILE = "globals.json"
+
+def load_globals():
+    if os.path.exists(GLOBALS_FILE):
+        try:
+            with open(GLOBALS_FILE, 'r') as f:
+                return json.load(f)
+        except:
+            return {}
+    return {}
+
+def save_globals(data):
+    with open(GLOBALS_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def gvarstatus(key):
+    data = load_globals()
+    return data.get(key)
+
+def addgvar(key, value):
+    data = load_globals()
+    data[key] = value
+    save_globals(data)
+
+def delgvar(key):
+    data = load_globals()
+    if key in data:
+        del data[key]
+        save_globals(data)
