2f6d1b5d667a3221039bcab2e5ba1039a07e404e
abd <bwdalmshrqy455@gmail.com>
2026-08-06 12:10:31 +0300

    Update managers.py

--- core/managers.py
+++ core/managers.py
@@ -1,1 +1,15 @@
+async def edit_delete(event, text, time=5):
+    try:
+        msg = await event.edit(text)
+        await asyncio.sleep(time)
+        await msg.delete()
+    except:
+        pass
 
+async def edit_or_reply(event, text):
+    try:
+        if event.out:
+            return await event.edit(text)
+        return await event.reply(text)
+    except:
+        return await event.reply(text)
